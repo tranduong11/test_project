d31006e152f2b0b9836f5b92b736c624343aaa0a
tranduong11 <duongttph50748@gmail.com>
2025-03-30 12:33:53 +0700

    commit 3

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:bloc_project/routers/app_routers.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      onGenerateRoute: AppRouter.instance.onGenerateRoute,
+    );
+  }
+}
